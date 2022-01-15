a6b783fb7187e301a6e0285c3e7873fcbfe59075
Nick <n.drobyshevski@pm.me>
2022-01-15 11:40:27 +0100

    hero animation fix, no movement along y coord

--- main.py
+++ main.py
@@ -4,57 +4,42 @@ from collections import deque
 
 class Hero:
     def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.skin_dict = {    'RIGHT_1' :[0,0,0,8,8],
-                           'LEFT_1' :[0,8,0,8,8],
-                          'RIGHT_2' :[0,0,8,8,8],
-                           'LEFT_2' :[0,8,8,8,8],
-                           'DOWN_1' :[0,0,32,8,8],
-                           'DOWN_2' :[0,8,32,8,8],
-                           'UP_1' :[0,0,24,8,8],
-                           'UP_2' :[0,8,24,8,8],
-                           'ATTACK_L_1': [0,16,0,16,8],
-                           'ATTACK_L_2': [0,16,8,16,8],
-                           'ATTACK_L_3': [0,16,16,16,8],
-                           
-                           }
-        self.skin = 'RIGHT_1'
-        self.att = False
+        self.x = 20
+        self.y = 20
+        self.active = False
+        self.frame = 0 
+        self.animation_frame = 0
+        self.height = 8
+        self.width = 8
+        self.u=0
+        
         self.sword = Sword()
     
     def draw(self):
-        pyxel.blt(self.x, self.y, *self.skin_dict[self.skin],000000)
+        pyxel.blt(self.x, self.y, img=0, u=self.u,v=self.height*self.animation_frame, w=self.width,h=self.height,colkey=000000)
         self.sword.draw()
      
     def update(self):
         if pyxel.btn(pyxel.KEY_LEFT):
-            self.move_animation('LEFT_1')
-            self.x = max(self.x - 2, 0)
+            self.u = 8
+            self.x = max(self.x - 1, 0)
+            
+            self.animation_frame = self.frame // 3 % 2
+            self.frame += 1
             
         if pyxel.btn(pyxel.KEY_RIGHT):
-            self.move_animation('RIGHT_1')
-            self.x = min(self.x + 2, pyxel.width - 16)
+            self.u = 0
+            self.x = min(self.x + 1, pyxel.width - 16)
+            self.animation_frame = self.frame // 3 % 2
+            self.frame += 1
             
-        if pyxel.btn(pyxel.KEY_DOWN):
-            self.move_animation('DOWN_1')
-            self.y = max(self.y + 2, 0)
-            
-        if pyxel.btn(pyxel.KEY_UP):
-            self.move_animation('UP_1')
-            self.y = min(self.y - 2, pyxel.width - 16)
+        # if pyxel.btn(pyxel.KEY_UP):
+        #     self.y = min(self.y - 2, pyxel.width - 16)
         
         if pyxel.btn(pyxel.KEY_Q):
             self.sword.set_visible()
-            
-        self.sword.update(self.x, self.y,self.skin)
-           
         
-    def move_animation(self, start_skin):
-        if "1" not in self.skin:
-            self.skin = start_skin[:-2] + "_1"
-        else: self.skin = start_skin[:-2] + "_2"
-    
+        self.sword.update(self.x, self.y,self.u)
     
     
 class Sword:
@@ -62,8 +47,6 @@ class Sword:
     def __init__(self):
         self.x = 20
         self.y = 20
-        self.skin = 'L_3'
-        self.direction = "LEFT"
         self.active = False
         self.frame = 0 
         self.animation_frame = 0
@@ -79,10 +62,10 @@ class Sword:
     
     def update(self,x,y,direction):
         self.y = y
-        if "LEFT" in direction:
+        if direction == 8:
             self.u = 16
             self.x = x-8
-        elif "RIGHT" in direction:
+        elif direction == 0:
             self.x = x + 8
             self.u = 24
         else: 
@@ -102,7 +85,7 @@ class Sword:
         self.active = False
 class App:
     def __init__(self):
-        pyxel.init(160, 120,fps=30)
+        pyxel.init(160, 120,fps=40)
         pyxel.load("my_resource.pyxres")
         self.hero = Hero()
         pyxel.run(self.update, self.draw)
